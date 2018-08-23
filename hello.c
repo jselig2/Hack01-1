2be9fb8893e8d4e7b176e7e1630ad1b007ca6684
jselig2 <42566584+jselig2@users.noreply.github.com>
2018-08-23 12:34:36 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char **argv) {
 
-  printf("Amanda Whitlatch, Computer Science\n");
+  printf("Amanda Whitlatch, Computer Science, Jackson Selig, Electrical Engineering\n");
 
   return 0;
 }
